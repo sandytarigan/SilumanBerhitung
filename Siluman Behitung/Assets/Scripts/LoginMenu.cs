11c7274d9b095bca31417b1623e005ae5b025346
haydariday <haydariday@gmail.com>
2019-12-12 15:36:59 +0700

    Revert "END YOUR LIFE"
    This reverts commit aca522f4a9554e63d97acf763a862a756f88a95e.

---
+++ Siluman Behitung/Assets/Scripts/LoginMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginMenu : MonoBehaviour
+{
+    public void GoToRegsiter()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Siluman Behitung/Assets/Scripts/RegistrationMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RegistrationMenu : MonoBehaviour
+{
+    public void GoToLogin()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
